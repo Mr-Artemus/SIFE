ec589c7c34e2cc40be75842a9c38868c7460e6f0
Mr-Artermus <31190188+Mr-Artemus@users.noreply.github.com>
2022-12-26 19:45:08 +0100

    More algorithms

--- src/wipe.rs
+++ src/wipe.rs
@@ -27,23 +27,63 @@ pub static ALGORITHMS: &[Algorithm] = &[
     Algorithm {
         name: "Simple pass of 0b00000000",
         writes: &[_write0],
-        checks: &[_check0],
+        checks: &[_check0]
     },
     Algorithm {
         name: "Simple pass of 0b11111111",
         writes: &[_write1],
-        checks: &[_check1],
+        checks: &[_check1]
     },
     Algorithm {
         name: "Simple pass of 0b01010101",
         writes: &[_write01],
-        checks: &[_check01],
+        checks: &[_check01]
     },
     Algorithm {
         name: "Simple pass of pseudo-random byte",
         writes: &[_writeRandom],
-        checks: &[_checkRandom],
+        checks: &[_checkRandom]
     },
+    Algorithm {
+        name: "GOST-R-50739-95 first version",
+        writes: &[_write0, _writeRandom],
+        checks: &[_check0, _checkRandom]
+    },
+    Algorithm {
+        name: "GOST-R-50739-95 second version",
+        writes: &[_writeRandom],
+        checks: &[_checkRandom]
+    },
+    Algorithm {
+        name: "HMG IS5",
+        writes: &[_write0, _writeRandom],
+        checks: &[_check0, _checkRandom]
+    },
+    Algorithm {
+        name: "HMG IS5 Enhanced",
+        writes: &[_write0, _write1, _writeRandom],
+        checks: &[_check0, _check1, _checkRandom]
+    },
+    Algorithm {
+        name: "RCMP TSSIT OPS-II",
+        writes: &[_write0, _write1, _write0, _write1, _write0, _write1, _writeRandom],
+        checks: &[_check0, _check1, _check0, _check1, _check0, _check1, _checkRandom]
+    },
+    Algorithm {
+        name: "AFSSI-5020",
+        writes: &[_write0, _write1, _writeRandom],
+        checks: &[_check0, _check1, _checkRandom]
+    },
+    Algorithm {
+        name: "AR 380-19",
+        writes: &[_writeRandom, _write1, _write0],
+        checks: &[_checkRandom, _check1, _check0]
+    },
+    Algorithm {
+        name: "Bruce Schneier Algorithm",
+        writes: &[_write0, _write1, _writeRandom, _writeRandom, _writeRandom, _writeRandom, _writeRandom],
+        checks: &[_check0, _check1, _checkRandom, _checkRandom, _checkRandom, _checkRandom, _checkRandom]
+    }
 ];
 
 static CLEAR_LINE: &'static str = "\x1b[2K\r";
